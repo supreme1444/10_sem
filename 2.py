dc2a3b8b84a8cb5d7c6d5124dc160e8ae9dd1327
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2022-10-09 12:57:25 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,16 @@
+class Road:
+    __length = 20
+    __width = 5
+    def ras(self):
+        res=self.__length*self.__width*25*5
+        print(res)
+        
+a = Road()
+a.ras()
+
+        
+
+
+        
+
+
